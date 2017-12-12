850839f36c54a728023d113a28d0a98920feba03
T-Midnight <belousovatanya1995@yandex.ru>
2017-12-12 23:17:58 +0400

    Add cookies(not finished), update layout(add managing buttons)

--- js/login.js
+++ js/login.js
@@ -2,7 +2,13 @@
 
 
 var tokenKey = "tokenInfo";
+if (getCookie("username")){
+		document.location.href = 'popup.html';
+	}else
+	{
 $(document).ready(function () {
+	
+	
     $('#submitLogin').click(function (e) {
         e.preventDefault();
         let email = $('#emailLogin').val();
@@ -11,6 +17,7 @@ $(document).ready(function () {
             console.log(success);
             sessionStorage.setItem(tokenKey, success["access_token"]);
             console.log(success["access_token"]);
+			setCookie("username", $('#emailLogin').val(), 60);
 			document.location.href = 'popup.html';
         },function (fail) {
             alert(fail);
@@ -18,3 +25,26 @@ $(document).ready(function () {
         // document.location.href = 'popup.html';
     });
 })
+	}
+function setCookie (name, value, expires) {
+      document.cookie = name + "=" + escape(value) + ((expires) ? "; expires=" + expires : "");
+}
+function getCookie(name) {
+	var cookie = " " + document.cookie;
+	var search = " " + name + "=";
+	var setStr = null;
+	var offset = 0;
+	var end = 0;
+	if (cookie.length > 0) {
+		offset = cookie.indexOf(search);
+		if (offset != -1) {
+			offset += search.length;
+			end = cookie.indexOf(";", offset)
+			if (end == -1) {
+				end = cookie.length;
+			}
+			setStr = unescape(cookie.substring(offset, end));
+		}
+	}
+	return(setStr);
+}
--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,9 @@
 var tokenKey = "tokenInfo";
+var token = sessionStorage.getItem(tokenKey);
+
 $(document).ready(function () {
     $('#add_task').click(function (e) {
         e.preventDefault();
-		let token = sessionStorage.getItem(tokenKey);
 		let text = $('#task').val();
 		let complete = 0;
         CTM.addTask(token, complete, text, function (success) {
@@ -12,25 +13,32 @@ $(document).ready(function () {
         }, function (fail) {
             alert(fail);
         });
-    });
-	
-    $('#button_done_all').click(function (e) {
-        e.preventDefault();
-		let token = sessionStorage.getItem(tokenKey);
-        CTM.getAllTasks(token, function (success) {
+		
+		CTM.getAllTasks(token, function (success) {
             $('#task_list').html(success);
             console.log(success);
             for (var i = 0; i < success.length; i++) {
-                $('#task_list').append('<li><input type="checkbox" name="task' + (i + 1) +'" value="a'+ (i + 1)+'"> <h6>' + success[i]['Text'] + '</h6></li>')
+                $('#task_list').append('<li><input type="checkbox" name="task' + (i + 1) +'" value="a'+ (i + 1)+'"><div class = "task_this"> <h6>' + success[i]['Text'] + '</h6>  </div> <input type = "button" value =" " class = "done"> <input type = "button" value =" " id = "edit_this" > <input type = "button" value =" " id = "remove_this"> </li>')
             }
         },function (fail) {
             alert(fail);
         });
     });
 	
+		
+	CTM.getAllTasks(token, function (success) {
+		$('#task_list').html(success);
+		console.log(success);
+		for (var i = 0; i < success.length; i++) {
+			$('#task_list').append('<li><input type="checkbox" name="task' + (i + 1) +'" value="a'+ (i + 1)+'"> <div class = "task_this"> <h6>' + success[i]['Text'] + '</h6> </div> <input type = "button" value =" " class = "done"> <input type = "button" value =" " id = "edit_this" > <input type = "button" value =" " id = "remove_this"> </li>')         
+		}
+	},function (fail) {
+		alert(fail);
+	});
+
+	
 	$('#button_refresh').click(function (e) {
         e.preventDefault();
-		let token = sessionStorage.getItem(tokenKey);
         let logOut = CTM.logOut(token, function (success) {
 			alert("Вы вышли из системы");
             sessionStorage.removeItem(tokenKey);
