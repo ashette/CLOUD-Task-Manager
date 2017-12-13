d88c39ecfdde6044007e1aa82b0338c5fa682132
Regener1 <st.8.sh@mail.ru>
2017-12-13 20:14:53 +0300

    create js task list

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,39 @@
 var tokenKey = "tokenInfo";
 var token = sessionStorage.getItem(tokenKey);
+var tasksList = [];
+
+function printTasks(tasksList) {
+    if(tasksList.length > 0){
+        $('#task_list').empty();
+    }
+    else{
+        return;
+    }
+    for (var i = 0; i < tasksList.length; i++) {
+        $('#task_list').append('<li><input type="hidden" name="id_task" value=\"'+ tasksList[i]['Id'] +'\">' +
+            '<input type="checkbox" name="task' + (i + 1) + '" value="a' + (i + 1) + '">' +
+            '<div class = "task_this"> <h6>' + tasksList[i]['Text'] + '</h6>  </div> ' +
+            '<input type = "button" value =" " class = "done"> ' +
+            '<input type = "button" value =" " id = "edit_this" >' +
+            ' <input type = "button" value =" " id = "remove_this"> </li>')
+    }
+
+
+}
+
+function handleTasks(success) {
+    if(success.length > 0){
+        tasksList.length = 0;
+    }
+    else{
+        return;
+    }
+
+    for (var i = 0; i < success.length; i++) {
+        tasksList.push(success[i]);
+    }
+    printTasks(tasksList);
+}
 
 $(document).ready(function () {
     $('#add_task').click(function (e) {
@@ -14,27 +48,17 @@ $(document).ready(function () {
             alert(fail);
         });
 
-        CTM.getAllTasks(token, function (success) {
-            $('#task_list').html(success);
-            console.log(success);
-            for (var i = 0; i < success.length; i++) {
-                $('#task_list').append('<li><input type="checkbox" name="task' + (i + 1) +'" value="a'+ (i + 1)+'"><div class = "task_this"> <h6>' + success[i]['Text'] + '</h6>  </div> <input type = "button" value =" " class = "done"> <input type = "button" value =" " id = "edit_this" > <input type = "button" value =" " id = "remove_this"> </li>')
-            }
-        },function (fail) {
+        CTM.getAllTasks(token, handleTasks,
+            function (fail) {
+                alert(fail);
+            });
+    });
+
+
+    CTM.getAllTasks(token, handleTasks,
+        function (fail) {
             alert(fail);
         });
-    });
-
-
-    CTM.getAllTasks(token, function (success) {
-        $('#task_list').html(success);
-        console.log(success);
-        for (var i = 0; i < success.length; i++) {
-            $('#task_list').append('<li><input type="checkbox" name="task' + (i + 1) +'" value="a'+ (i + 1)+'"> <div class = "task_this"> <h6>' + success[i]['Text'] + '</h6> </div> <input type = "button" value =" " class = "done"> <input type = "button" value =" " id = "edit_this" > <input type = "button" value =" " id = "remove_this"> </li>')
-        }
-    },function (fail) {
-        alert(fail);
-    });
 
 
     $('#button_refresh').click(function (e) {
@@ -45,7 +69,7 @@ $(document).ready(function () {
             deleteCookie("username");
             deleteCookie("password");
             document.location.href = 'login.html';
-        },function (fail) {
+        }, function (fail) {
             alert(fail);
         });
     });
