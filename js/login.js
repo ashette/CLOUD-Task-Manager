038e0588f386f79aa5adb4a275be1131677bc8b3
Regener1 <st.8.sh@mail.ru>
2017-12-13 19:36:48 +0300

    add cookie

--- js/login.js
+++ js/login.js
@@ -2,49 +2,40 @@
 
 
 var tokenKey = "tokenInfo";
-if (getCookie("username")){
-		document.location.href = 'popup.html';
-	}else
-	{
-$(document).ready(function () {
-	
-	
-    $('#submitLogin').click(function (e) {
-        e.preventDefault();
-        let email = $('#emailLogin').val();
-        let password = $('#passwordLogin').val();
-        let loginInfo = CTM.login(email, password, function (success) {
-            console.log(success);
-            sessionStorage.setItem(tokenKey, success["access_token"]);
-            console.log(success["access_token"]);
-			setCookie("username", $('#emailLogin').val(), 60);
-			document.location.href = 'popup.html';
-        },function (fail) {
-            alert(fail);
-        });
-        // document.location.href = 'popup.html';
+var user = getCookie("username");
+var password = getCookie("password");
+if (user || password){
+    let email = getCookie("username");
+    let password = getCookie("password");
+    let loginInfo = CTM.login(email, password, function (success) {
+        console.log(success);
+        sessionStorage.setItem(tokenKey, success["access_token"]);
+        console.log(success["access_token"]);
+        document.location.href = 'popup.html';
+    },function (fail) {
+        alert(fail);
     });
-})
-	}
-function setCookie (name, value, expires) {
-      document.cookie = name + "=" + escape(value) + ((expires) ? "; expires=" + expires : "");
+
+}else
+{
+    $(document).ready(function () {
+
+        $('#submitLogin').click(function (e) {
+            e.preventDefault();
+            let email = $('#emailLogin').val();
+            let password = $('#passwordLogin').val();
+            let loginInfo = CTM.login(email, password, function (success) {
+                console.log(success);
+                sessionStorage.setItem(tokenKey, success["access_token"]);
+                console.log(success["access_token"]);
+                setCookie("username", $('#emailLogin').val());
+                setCookie("password", $('#passwordLogin').val());
+                document.location.href = 'popup.html';
+            },function (fail) {
+                alert(fail);
+            });
+            // document.location.href = 'popup.html';
+        });
+    })
 }
-function getCookie(name) {
-	var cookie = " " + document.cookie;
-	var search = " " + name + "=";
-	var setStr = null;
-	var offset = 0;
-	var end = 0;
-	if (cookie.length > 0) {
-		offset = cookie.indexOf(search);
-		if (offset != -1) {
-			offset += search.length;
-			end = cookie.indexOf(";", offset)
-			if (end == -1) {
-				end = cookie.length;
-			}
-			setStr = unescape(cookie.substring(offset, end));
-		}
-	}
-	return(setStr);
-}
+
--- js/popup.js
+++ js/popup.js
@@ -4,17 +4,17 @@ var token = sessionStorage.getItem(tokenKey);
 $(document).ready(function () {
     $('#add_task').click(function (e) {
         e.preventDefault();
-		let text = $('#task').val();
-		let complete = 0;
+        let text = $('#task').val();
+        let complete = 0;
         CTM.addTask(token, complete, text, function (success) {
-			alert(success);
-			alert("Вы что-то добавили");
+            alert(success);
+            alert("Вы что-то добавили");
             console.log(success);
         }, function (fail) {
             alert(fail);
         });
-		
-		CTM.getAllTasks(token, function (success) {
+
+        CTM.getAllTasks(token, function (success) {
             $('#task_list').html(success);
             console.log(success);
             for (var i = 0; i < success.length; i++) {
@@ -24,25 +24,27 @@ $(document).ready(function () {
             alert(fail);
         });
     });
-	
-		
-	CTM.getAllTasks(token, function (success) {
-		$('#task_list').html(success);
-		console.log(success);
-		for (var i = 0; i < success.length; i++) {
-			$('#task_list').append('<li><input type="checkbox" name="task' + (i + 1) +'" value="a'+ (i + 1)+'"> <div class = "task_this"> <h6>' + success[i]['Text'] + '</h6> </div> <input type = "button" value =" " class = "done"> <input type = "button" value =" " id = "edit_this" > <input type = "button" value =" " id = "remove_this"> </li>')         
-		}
-	},function (fail) {
-		alert(fail);
-	});
 
-	
-	$('#button_refresh').click(function (e) {
+
+    CTM.getAllTasks(token, function (success) {
+        $('#task_list').html(success);
+        console.log(success);
+        for (var i = 0; i < success.length; i++) {
+            $('#task_list').append('<li><input type="checkbox" name="task' + (i + 1) +'" value="a'+ (i + 1)+'"> <div class = "task_this"> <h6>' + success[i]['Text'] + '</h6> </div> <input type = "button" value =" " class = "done"> <input type = "button" value =" " id = "edit_this" > <input type = "button" value =" " id = "remove_this"> </li>')
+        }
+    },function (fail) {
+        alert(fail);
+    });
+
+
+    $('#button_refresh').click(function (e) {
         e.preventDefault();
         let logOut = CTM.logOut(token, function (success) {
-			alert("Вы вышли из системы");
+            alert("Вы вышли из системы");
             sessionStorage.removeItem(tokenKey);
-			document.location.href = 'login.html';
+            deleteCookie("username");
+            deleteCookie("password");
+            document.location.href = 'login.html';
         },function (fail) {
             alert(fail);
         });
