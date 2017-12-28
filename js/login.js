694486a393ad1a4d992041b39c3635b5de51b0c0
T-Midnight <belousovatanya1995@yandex.ru>
2017-12-28 23:23:33 +0400

    Fix Cookie

--- js/login.js
+++ js/login.js
@@ -4,9 +4,6 @@
 var tokenKey = "tokenInfo";
 var user = getCookie("username");
 var password = getCookie("password");
-
-//var xhr = new XMLHttpRequest();
-
 if (user || password){
     let email = getCookie("username");
     let password = getCookie("password");
@@ -28,10 +25,16 @@ if (user || password){
             let email = $('#emailLogin').val();
             let password = $('#passwordLogin').val();			
 			let loginInfo = CTM.login(email, password, function (success) {
-				sessionStorage.setItem(tokenKey, success["access_token"]);
-				setCookie("username", $('#emailLogin').val());
-				setCookie("password", $('#passwordLogin').val());
-				document.location.href = 'popup.html';
+				console.log(success);
+                sessionStorage.setItem(tokenKey, success["access_token"]);
+                console.log(success["access_token"]);
+
+				let ch = $("#checkboxMe").attr("checked");
+				if($("#checkboxMe").prop("checked")){
+                setCookie("username", $('#emailLogin').val(), 7);
+                setCookie("password", $('#passwordLogin').val(),7);
+				}
+                document.location.href = 'popup.html';
 				},function (fail) {
 					alert(fail);
 			});
